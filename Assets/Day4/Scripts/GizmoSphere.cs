e83dda4e2c266ca1e0739cda6880599f8a3d9a7a
Troy Sanders <troyjsanders@gmail.com>
2018-10-04 08:20:18 -0500

    Inktober Day #4
    Gizmo spheres

---
+++ Assets/Day4/Scripts/GizmoSphere.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GizmoSphere : MonoBehaviour {
+
+    #region Public Variables
+    public float radius = 2;
+    // Translucent Green by Default
+    public Color selectedColor = new Color(0, 1, 0, .75f);
+    public Color defaultColor = new Color(1, 0, 0, .75f);
+    #endregion
+
+#if UNITY_EDITOR
+    public void OnDrawGizmos()
+    {
+        if (UnityEditor.Selection.activeGameObject == gameObject)
+        {            
+            Gizmos.color = selectedColor;
+            Gizmos.DrawSphere(transform.position, radius);
+        }
+        else
+        {
+            Gizmos.color = defaultColor;
+            Gizmos.DrawSphere(transform.position, radius);
+        }
+    }
+#endif
+}
